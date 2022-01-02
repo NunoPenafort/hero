0e105854acc83c927fc621bfe99d04e15ecd3109
Nuno Penafort <up202008405@up.pt>
2022-01-02 15:21:26 +0000

    Game class -> Initializes Lanterna terminal and screen; -> Added draw() method to paint screen

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Game {
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private TerminalScreen screen;
+    public Game() {
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            this.screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void draw() throws IOException{
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+
+    }
+    public void run() {
+
+    }
+
 }
